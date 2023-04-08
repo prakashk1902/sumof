e26e5d5bffde1eff16c45663e357b477c5fb9dd7
prakashk1902 <59604735+prakashk1902@users.noreply.github.com>
2023-04-08 16:55:39 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,14 @@
+package my_pro;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		int x=1, sum=0;
+		while(x<=10) {
+			sum=sum+x;
+			x++;
+		}
+    System.out.println("sum of first 10 natural no is:"+sum);
+	}
+
+}
